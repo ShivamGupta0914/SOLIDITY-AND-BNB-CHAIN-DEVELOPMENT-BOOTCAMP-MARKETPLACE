03d76d5042cb5f558069a08f1f730b030c2eff9d
shivam7868-alt <shivamgupta14092002@gmail.com>
2023-09-24 15:40:07 +0530

    refactor: updated config file for bsc testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.19",
-    networks: {
-      bsctestnet: {
-        url: process.env.BSC_ARCHIVE_NODE_URL || "https://data-seed-prebsc-1-s1.binance.org:8545",
-        chainId: 97,
-        accounts: {
-          mnemonic: process.env.MNEMONIC || "",
-        },
+  solidity: "0.8.18",
+  namedAccounts: {
+    deployer: {
+      default: 0, // ethers built in account at index 0
+    },
+  },
+  networks: {
+    bsctestnet: {
+      url: process.env.BSC_ARCHIVE_NODE_URL || "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
       },
     },
-    etherscan: {
-      apiKey: "Z1NGNIDYG42JMMSBWCC6V3G19EXBZSWFNQ"
-    }
-}
+  },
+  etherscan: {
+    apiKey: process.env.BSC_API_KEY
+  }
+};
